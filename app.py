da44f4a02fbc4d01c2c6b59a58b65768bd22625a
Shafif Ferdous Rahman <101551508+Shafif090@users.noreply.github.com>
2025-01-09 13:32:13 +0000

    Created basic structure for Auth page

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login")
+def home():
+    return render_template("login.html")
+
+@app.route("/register")
+def home():
+    return render_template("login.html")
